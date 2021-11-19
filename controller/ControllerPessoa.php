7fba06f31d9db7e4f30f37ed778873620c3529f7
GUSTAVO DE OLIVEIRA PEREIRA <21193137@CFP127ALUNO.LOCAL>
2021-11-19 11:21:54 -0300

    POST criado

--- controller/ControllerPessoa.php
+++ controller/ControllerPessoa.php
@@ -1,25 +1,61 @@
 <?php
 
 //Criando uma classe
-    class controllerPessoa{
+    class ControllerPessoa{
 
         private $_method;
         private $_modelPessoa;
+        private $_codPessoa;
         
         public function __construct($model){
 
             $this->_modelPessoa = $model;
             $this->_method = $_SERVER['REQUEST_METHOD'];
 
+            //PERMITE RECEBER DADOS JSON ATRAVÉS DA REQUISIÇÃO
+            $json = file_get_contents("php://input");
+
+            //echo $json;exit;
+            $dadosPessoa = json_decode($json);
+
+            //SE TIVER UM codPessoa ELE ENVIA O codPessoa,
+            //SE NÃO ELE ENVIA NULO
+            $this->_codPessoa = $dadosPessoa->cod_pessoa ?? null;
+
         }
 
         public function router(){
 
             switch ($this->_method) {
-                case 'value':
-                    # code...
+                case 'GET':
+
+                // echo $this->_codPessoa;exit;
+                    
+                    if (isset($this->_codPessoa)) {
+
+                        //SEMPRE QUE RODAR UM return ELE NÃO IRÁ LER O RESTO
+                        return $this->_modelPessoa->findById();                   
+
+                    }
+
+                    return $this->_modelPessoa->findAll();
+
                     break;
                 
+                case 'POST':
+                    
+                    return $this->_modelPessoa->create();
+                    break;
+                    
+                    break;
+                case 'PUT':
+                    # code...
+                    break;
+                case 'DELETE':
+                    # code...
+                break;
+
+
                 default:
                     # code...
                     break;
--- model/ModelPessoa.php
+++ model/ModelPessoa.php
@@ -5,9 +5,39 @@
 
         private $_conn;
 
+        private $_codPessoa;
+        private $_nome;
+        private $_sobrenome;
+        private $_email;
+        private $_celular;
+        private $_fotografia;
+
+    
         public function __construct($conn){
 
+            //PERMITE RECEBER DADOS JSON ATRAVÉS DA REQUISIÇÃO
+            //IRÁ RECEBER DE QUALQUER ENTRADA 
+            $json = file_get_contents("php://input");
+
+            $dadosPessoa = json_decode($json);
+
+            
+            //echo $dadosPessoa;exit;
+
+            //Irá receber o codPessoa do POSTMAN que tem 
+            //um atributo com esse nome e irá guardar em _codPessoa
+
+            //RECEBIMENTO DOS DADOS DO POSTMAN
+            $this->_codPessoa = $dadosPessoa->cod_pessoa ?? null;
+            $this->_nome = $dadosPessoa->nome ?? null;
+            $this->_sobrenome = $dadosPessoa->sobrenome ?? null;
+            $this->_email = $dadosPessoa->email ?? null;
+            $this->_celular = $dadosPessoa->celular ?? null;
+            $this->_fotografia = $dadosPessoa->fotografia ?? null;
+
             $this->_conn = $conn;
+
+
 
         }
         public function findAll(){
@@ -22,9 +52,46 @@
             $statment->execute();
 
             //DEVOLVE OS VALORES DA SELECT PARA SEREM UTILIZADOS
-            return $statment->fetchAll();
+            return $statment->fetchAll(\PDO::FETCH_ASSOC);
         }
 
+        public function findById(){
+
+            $sql = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = ?";
+
+            $statment = $this->_conn->prepare($sql);
+            $statment->bindValue(1, $this->_codPessoa);
+
+            $statment->execute();
+
+            return $statment->fetchAll(\PDO::FETCH_ASSOC);
+
+
+        }
+        public function create(){
+
+            $sql = "INSERT INTO tbl_pessoa (nome, sobrenome, email, celular, fotografia)
+            VALUES (?, ?, ?, ?, ?)";
+
+            $statment = $this->_conn->prepare($sql);
+
+            $statment->bindValue(1, $this->_nome);
+            $statment->bindValue(2, $this->_sobrenome);
+            $statment->bindValue(3, $this->_email);
+            $statment->bindValue(4, $this->_celular);
+            $statment->bindValue(5, $this->_fotografia);
+            
+
+
+            if ($statment->execute()) {
+                return "Success";            
+               }
+               else{
+                   return "Error";
+               }
+
+
+        }
 
     }
 
